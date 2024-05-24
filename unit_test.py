7777948a5dcdddc7b494e5af4afb7f4993c606cf
Shane-shy <1602640697@qq.com>
2024-05-24 19:38:00 +0800

    update

--- unit_test.py
+++ unit_test.py
@@ -5,7 +5,7 @@ from img_transfer import find_md_img
 class MyTestCase(unittest.TestCase):
     def test_find_md_img(self, ):
         # self.assertEqual(True, False)  # add assertion here
-        md_path = '/home/shaneshi/Documents/Notes/test.md'
+        md_path = '/home/shaneshi/Documents/Notes/daily_notes/markdown上传博客园.md'
         with open(md_path, encoding='utf-8') as f:
             md = f.read()
             self.assertEqual(find_md_img(md), [])
